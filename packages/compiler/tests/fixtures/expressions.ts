634cd316ed252373388f198f1506e1be46531442
Chenxin Yan <yanchenxin2004@gmail.com>
2026-08-20 20:47:54 -0400

    fix(compiler): reject unsafe console coercions

--- packages/compiler/tests/fixtures/expressions.ts
+++ packages/compiler/tests/fixtures/expressions.ts
@@ -1,4 +1,4 @@
 console.log(6 * 7, 5 / 2, 11 % 4);
 console.log(0.1 + 0.2, -0);
 console.log(1 / (process.argv.slice(2).length - 1));
-console.log("hello " + process.argv.slice(2)[0], "answer=" + 42);
+console.log("values:", "hello " + process.argv.slice(2)[0], "answer=" + 42);
